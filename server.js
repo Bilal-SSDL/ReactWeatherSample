f645fbc4ddcb91a11f02d26103070351516c8219
Bilal Zulfiqar <bilal.abbasi@seventechnology.co.uk>
2018-09-24 19:04:02 +0500

    Added Heroku Configurations

--- server.js
+++ server.js
@@ -3,6 +3,16 @@ var express = require('express');
 //create our app
 
 var app = express();
+const PORT = process.env.PORT  || 3000;
+
+
+app.use(function (req, res, next){
+    if(req.headers['x-forwarded-proto'] === 'http'){
+        next();
+    }else {
+        res.redirect('http://' + req.hostname + req.url);
+    }
+});
 
 // tell the folder name which we want to expose
 
@@ -10,6 +20,6 @@ app.use(express.static('public'));
 
 // start the server
 
-app.listen(3000, function() {
-    console.log("Server has been started on port 3000");
+app.listen(PORT, function() {
+    console.log("Server has been started on port " + PORT);
 });
