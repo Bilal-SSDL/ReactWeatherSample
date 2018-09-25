80c066592ab8ba8dee2a0afcef74bc30c3f7cfa4
Bilal Zulfiqar <bilal.abbasi@seventechnology.co.uk>
2018-09-25 13:48:11 +0500

    Added stle in Main and Example component

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,4 +1,5 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 // var Examples= React.createClass({
 //     render: function () {
@@ -9,8 +10,23 @@ var React = require('react');
 // });
 
 var Examples= (props) => {
-    return (       
-    <h2> Examples </h2>
+    return (
+        <div>      
+    <h1 className="text-center"> Examples </h1>
+    <p> Here are some Examples locations to try out </p>
+
+        <ol>
+            <li>
+                <Link to='/?location=Islamabad'>Islamabad</Link>
+            </li>
+            <li>
+                <Link to='/?location=Lahore'>Lahore</Link>
+            </li>
+            <li>
+                <Link to='/?location=Karachi'>Karachi</Link>
+            </li>
+        </ol>
+        </div>
    );
 }
 
