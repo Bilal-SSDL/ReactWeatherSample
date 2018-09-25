0ea5f48ef66a60184ef05348d509bcb1866ccbb0
Bilal Zulfiqar <bilal.abbasi@seventechnology.co.uk>
2018-09-25 15:02:59 +0500

    Added style to About

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -9,8 +9,25 @@ var React = require('react');
 // });
 
 var About = (props) => {
-    return (       
-        <h2> About Component </h2>
+    return (   
+        <div>    
+        <h1 className="text-center"> About Component </h1>
+        <p>
+            This is the weather application build on React. I have complete this for practicing my react skills.
+        </p>
+        <p>
+            Here are the links of some of the tools and technologies whic I used for my app.
+        </p>
+        <ul>
+            <li>
+                <a href="https://facebook.github.io/react">React</a> - This was the javascript framework used
+            </li>
+            <li>
+                <a href="http://openweathermap.org">Open Weather Map </a> - I used Open Weather Map to search for weather data by city name.
+            </li>
+        </ul>
+        </div>
+
    );
     
 }
